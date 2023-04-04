6404b417b7f41fb3ea329cfecfc0fe72c409eff2
mordakin <chumik_2002@mail.ru>
2023-04-04 23:10:34 +0300

    Create first urls and 404

--- chumachechy/chumachechy/urls.py
+++ chumachechy/chumachechy/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bank.views import index, pageNotFound
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('bank.urls'))
 ]
+
+handler404 = pageNotFound
