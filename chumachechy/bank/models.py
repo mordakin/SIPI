1ecc9abd63053d92d873caa67f358631adb10d69
mordakin <chumik_2002@mail.ru>
2023-04-13 21:45:45 +0300

    create databases 0.0.3

--- chumachechy/bank/models.py
+++ chumachechy/bank/models.py
@@ -1,3 +1,26 @@
+"""Создание моделей(БД)"""
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    """Данные пользователя"""
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.IntegerField(unique=True)
+    passport = models.BigIntegerField(max_length=10, unique=True)
+    fio = models.CharField(max_length=255, unique=True)
+
+
+class BankAccount(models.Model):
+    """Данные о счёте"""
+    account_number = models.BigIntegerField(
+        unique=True, null=True)  # номер счёта
+    amount_of_funds = models.IntegerField(default=0)  # средств на счету
+    account_user = models.ForeignKey('UserData', on_delete=models.PROTECT)
+
+
+class Transfer(models.Model):
+    """Данные о переводах"""
+    sender_name = models.CharField(max_length=255)  # имя отправителя
+    recipient_name = models.CharField(max_length=255)  # имя получателя
+    cost = models.IntegerField  # сумма перевода
