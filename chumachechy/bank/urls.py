5284349cd18fc7627a7398c645480fb366710b86
mordakin <chumik_2002@mail.ru>
2023-04-13 18:04:36 +0300

    created primary routing

--- chumachechy/bank/urls.py
+++ chumachechy/bank/urls.py
@@ -1,7 +1,13 @@
+"""Маршрутизация"""
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('accounts', accounts, name='accounts'),
+    path('transfer', transfer, name='transfer'),
+    path('translation_history', translation_history, name='translation_history'),
+    path('login', login, name='login'),
+    path('sing_in', sing_in, name='sing_in'),
 
 ]
--- chumachechy/bank/views.py
+++ chumachechy/bank/views.py
@@ -8,6 +8,31 @@ def index(request):
     return render(request, 'bank/index.html', {'title': 'Главная страница'})
 
 
+def accounts(request):
+    """Страница счетов"""
+    return render(request, 'bank/accounts.html', {'title': 'Счета'})
+
+
+def transfer(request):
+    """Страницы переводов"""
+    return render(request, 'bank/transfer.html', {'title': 'Переводы'})
+
+
+def translation_history(request):
+    """Страница истории переводов"""
+    return render(request, 'bank/translation_history.html', {'title': 'История переводов'})
+
+
+def login(request):
+    """Авторизация"""
+    return render(request, 'bank/login.html', {'title': 'Вход'})
+
+
+def sing_in(request):
+    """Регистрация"""
+    return render(request, 'bank/sing_in.html', {'title': 'Регистрация'})
+
+
 def pageNotFound(request, exception):
     """Страница не найдена"""
     return HttpResponseNotFound('Такой страницы нет((')
